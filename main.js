292af273ddd25e3ee146d21983098dcaf636fb5e
Maks Bessonov <max8essonov@gmail.com>
2025-04-04 15:30:08 +0500

    initialize project with HTML, CSS, and JavaScript files

---
+++ main.js
@@ -0,0 +1,36 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    console.log('You must use the "new" operator to call the constructor');
+  }
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.id = crypto.randomUUID()),
+    (this.info = () => {
+      return [this.title, this.author, `${this.pages} pages`, this.read];
+    });
+}
+
+Book.prototype.review = function () {
+  return `${this.title} - хуйня собачья`;
+};
+
+function addBookToTheLibrary(title, author, pages, read) {
+  if (!title) {
+    throw new Error("you should specify the title");
+  }
+  if (!author) {
+    throw new Error("you should specify the author");
+  }
+  if (!pages) {
+    throw new Error("you should specify the pages");
+  }
+  if (!read) {
+    throw new Error("you should specify if you read it");
+  }
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
