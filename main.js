b5fe75e7e6752ce0fc8582ee35232cfa8cf41980
Maks Bessonov <socialnerd1488@gmail.com>
2025-04-07 22:49:37 +0500

    delete some unnecessary code, move testbooks to a separate file

--- main.js
+++ main.js
@@ -1,48 +1,10 @@
+import { testBooks } from "./testbooks.js";
 const myLibrary = [];
 const libary = document.querySelector(".lib");
 const addBookBtn = document.querySelector(".add-book");
 const bookFormContainer = document.querySelector(".book-form-container");
 const bookForm = document.querySelector(".book-form");
 const closeForm = document.querySelector(".close-form");
-
-const testBooks = [
-  {
-    title: "Harry Potter and Sorcerer's Stone",
-    author: "J.K. Rowling",
-    pages: 223,
-    read: true,
-    description:
-      "Harry Potter and the Sorcerer's Stone is a fantasy novel written by British author J.K. Rowling. It follows the life of a young wizard, Harry Potter, who discovers his magical heritage and attends Hogwarts School of Witchcraft and Wizardry. Along with his friends Ron Weasley and Hermione Granger, Harry faces various challenges, including the dark wizard Lord Voldemort, who seeks to regain power. The book explores themes of friendship, bravery, and the battle between good and evil.",
-    imgSrc: "./assets/harry_potter_and_sorcerer's_stone.jpg",
-  },
-  {
-    title: "The Fellowship of the Ring",
-    author: "J.R.R. Tolkien",
-    pages: 423,
-    read: false,
-    description:
-      "In the first part of The Lord of the Rings trilogy, young hobbit Frodo Baggins inherits a powerful ring that must be destroyed to save Middle-earth. Joined by a fellowship of allies, he begins a perilous journey to Mount Doom, facing ancient evils and growing darkness along the way.",
-    imgSrc: "./assets/the_fellowship_of_the_ring.jpg",
-  },
-  {
-    title: "Roadside Picnic",
-    author: "A. & B. Strugatsky",
-    pages: 224,
-    read: true,
-    description:
-      "After a mysterious alien visitation leaves behind dangerous and otherworldly zones, humanity struggles to understand the strange phenomena. Redrick “Red” Schuhart, a stalker who illegally ventures into the Zone to retrieve alien artifacts, risks everything for knowledge, survival, and hope in a world forever changed.",
-    imgSrc: "./assets/roadside_picnic.jpg",
-  },
-  {
-    title: "Angels & Demons",
-    author: "Dan Brown",
-    pages: 768,
-    read: true,
-    description:
-      "When a physicist is murdered and a mysterious symbol is found branded on his chest, symbologist Robert Langdon is drawn into a secret war between science and religion. Racing through the heart of Vatican City, Langdon must stop a deadly plot linked to the ancient brotherhood of the Illuminati.",
-    imgSrc: "./assets/angels_and_demons.jpg",
-  },
-];
 
 addBookBtn.addEventListener("click", () => {
   bookFormContainer.classList.add("open-book-container");
@@ -166,16 +128,6 @@ bookForm.addEventListener("submit", (e) => {
   let read = null;
   form.readStatus.value === "read" ? (read = true) : (read = false);
   const coverFile = form.cover.files[0];
-
-  console.log(typeof form);
-
-  function clearFields() {
-    form.title.value = "";
-    form.author.value = "";
-    form.pages.value = "";
-    form.description.value = "";
-    form.cover.files[0] = "";
-  }
 
   if (coverFile) {
     const reader = new FileReader();
---
+++ testbooks.js
@@ -0,0 +1,38 @@
+export const testBooks = [
+  {
+    title: "Harry Potter and Sorcerer's Stone",
+    author: "J.K. Rowling",
+    pages: 223,
+    read: true,
+    description:
+      "Harry Potter and the Sorcerer's Stone is a fantasy novel written by British author J.K. Rowling. It follows the life of a young wizard, Harry Potter, who discovers his magical heritage and attends Hogwarts School of Witchcraft and Wizardry. Along with his friends Ron Weasley and Hermione Granger, Harry faces various challenges, including the dark wizard Lord Voldemort, who seeks to regain power. The book explores themes of friendship, bravery, and the battle between good and evil.",
+    imgSrc: "./assets/harry_potter_and_sorcerer's_stone.jpg",
+  },
+  {
+    title: "The Fellowship of the Ring",
+    author: "J.R.R. Tolkien",
+    pages: 423,
+    read: false,
+    description:
+      "In the first part of The Lord of the Rings trilogy, young hobbit Frodo Baggins inherits a powerful ring that must be destroyed to save Middle-earth. Joined by a fellowship of allies, he begins a perilous journey to Mount Doom, facing ancient evils and growing darkness along the way.",
+    imgSrc: "./assets/the_fellowship_of_the_ring.jpg",
+  },
+  {
+    title: "Roadside Picnic",
+    author: "A. & B. Strugatsky",
+    pages: 224,
+    read: true,
+    description:
+      "After a mysterious alien visitation leaves behind dangerous and otherworldly zones, humanity struggles to understand the strange phenomena. Redrick “Red” Schuhart, a stalker who illegally ventures into the Zone to retrieve alien artifacts, risks everything for knowledge, survival, and hope in a world forever changed.",
+    imgSrc: "./assets/roadside_picnic.jpg",
+  },
+  {
+    title: "Angels & Demons",
+    author: "Dan Brown",
+    pages: 768,
+    read: true,
+    description:
+      "When a physicist is murdered and a mysterious symbol is found branded on his chest, symbologist Robert Langdon is drawn into a secret war between science and religion. Racing through the heart of Vatican City, Langdon must stop a deadly plot linked to the ancient brotherhood of the Illuminati.",
+    imgSrc: "./assets/angels_and_demons.jpg",
+  },
+];
